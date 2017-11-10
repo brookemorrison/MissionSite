249130ea841b8cecc39a6ce0240543dfbc6c581d
brookemorrison <32530763+brookemorrison@users.noreply.github.com>
2017-11-10 09:26:34 -0700

    First commit

---
+++ MissionSite/Controllers/MissionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MissionSite.Controllers
+{
+    public class MissionsController : Controller
+    {
+        // GET: Missions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Missions
+        //passes parameters to the MissionInfo view when it gets directed towards
+        public ActionResult MissionInfo(string Language, String MissionName, string PresidentName, string Address, string Climate, string Religion, string Picture)
+        {
+            ViewBag.MissionName = MissionName;
+            ViewBag.PresidentName = PresidentName;
+            ViewBag.Address = Address;
+            ViewBag.Climate = Climate;
+            ViewBag.Religion = Religion;
+            ViewBag.Picture = Picture;
+            ViewBag.Language = Language;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FAQAnswers(FormCollection form)
+        {
+            // String reply = form["New Reply"].ToString();
+            // form.Clear();
+            return View();
+        }
+    }
+}
